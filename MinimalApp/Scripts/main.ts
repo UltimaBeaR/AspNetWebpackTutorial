ca84f321670a158ca80ce7c705570633d0e52aaf
UltimaBeaR <thebear@yandex.ru>
2017-06-03 18:02:44 +0300

    Added minimal possible working angular 4 configuration + some common project changes

--- MinimalApp/Scripts/main.ts
+++ MinimalApp/Scripts/main.ts
@@ -1,1 +1,33 @@
-﻿document.getElementById("helloworld").innerText = "Hello world from script";
+﻿// Определение необходимых вещей, без которых angular работать не захочет
+
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+
+// Определение основного angular компонента (компонент для отображения фразы hello world)
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'hello-world',
+    template: `<h1>Hello world from {{name}}!</h1>`
+})
+class HelloWorldComponent { name = 'angular'; }
+
+// Определение основного angular модуля
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [HelloWorldComponent],
+    bootstrap: [HelloWorldComponent]
+})
+class HelloWorldModule { }
+
+// Загрузка angular приложения (указывается основной angular модуль для загрузки)
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+platformBrowserDynamic().bootstrapModule(HelloWorldModule);
--- MinimalApp/webpack.config.js
+++ MinimalApp/webpack.config.js
@@ -1,40 +1,65 @@
 ﻿"use strict"
-{
-    // Требуется для формирования полного output пути
-    let path = require('path');
 
-    // Плагин для очистки выходной папки (bundle) перед созданием новой
-    const CleanWebpackPlugin = require('clean-webpack-plugin');
+// Сам webpack, для доступа к стандартным плагинам webpack-а
+var webpack = require('webpack');
 
-    // Путь к выходной папке
-    const bundleFolder = "wwwroot/bundle/";
+// Требуется для формирования полного output пути
+let path = require('path');
 
-    module.exports = {
-        // Точка входа в приложение
-        entry: "./Scripts/main.ts",
+// Плагин для отслеживания в консоли момента, когда вебпак начинает построение
+const watchTimePlugin = require('webpack-watch-time-plugin');
 
-        // Выходной файл
-        output: {
-            filename: 'script.js',
-            path: path.resolve(__dirname, bundleFolder)
+// Плагин для очистки выходной папки (bundle) перед созданием новой
+const CleanPlugin = require('clean-webpack-plugin');
+
+// Папка с входными (исходными) файлами скриптов
+const scriptsFolderName = "Scripts";
+
+// Путь к выходной папке
+const bundleFolder = "wwwroot/bundle/";
+
+module.exports = {
+    // Абсолютная папка, относительно которой ищутся точки входа
+    context: path.resolve(__dirname, scriptsFolderName),
+
+    // Точки входа в приложение
+    entry: {
+        main: './main.ts',
+    },
+
+    // Выходные файлы
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, bundleFolder)
+    },
+    module: {
+        rules: [
+        {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
         },
-        module: {
-          rules: [
-            {
-              test: /\.tsx?$/,
-              loader: "ts-loader",
-              exclude: /node_modules/,
-            },
-          ]
-        },
-        resolve: {
-            extensions: [".tsx", ".ts", ".js"]
-        },
-        plugins: [
-            new CleanWebpackPlugin([bundleFolder])
-        ],
-        // Включаем генерацию отладочной информации внутри выходного файла
-        // (Нужно для работы отладки клиентских скриптов)
-        devtool: "inline-source-map"
-    };
-}
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    plugins: [
+        // Пишет в консоль время, когда вебпак начинает перестроение (полезно для watch режима)
+        watchTimePlugin,
+
+        // Очищение папки bundleFolder каждый раз перед перестроением
+        new CleanPlugin([bundleFolder]),
+
+        // Обход ворнингов из ангуляра issue 11580 в github репозитории ангуляра
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)@angular/,
+            // ToDo: Не уверен что эта строка должна быть именно такой, ворнинг исчезает при любом пути тут
+            path.resolve(__dirname, scriptsFolderName),
+            {}
+        ),
+    ],
+    // Включаем генерацию отладочной информации внутри выходного файла
+    // (Нужно для работы отладки клиентских скриптов)
+    devtool: "source-map"
+};
