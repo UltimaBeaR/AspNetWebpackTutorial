dfe71b8738b82cb690c9681f4f4e97afc26c5f4f
UltimaBeaR <thebear@yandex.ru>
2017-06-04 18:47:19 +0300

    Hello world component and main angular module are now in separate files/modules. Added sass support.

---
+++ MinimalApp/Frontend/ngApp/Components/helloWorld/helloWorld.ts
@@ -0,0 +1,23 @@
+﻿// Определение основного angular компонента (компонент для отображения фразы hello world)
+
+import { Component } from '@angular/core';
+
+import template from './template.html';
+import style from './style.scss';
+
+@Component({
+    selector: 'hello-world',
+    template: template,
+    styles: [style]
+})
+export class MainComponent {
+    hello: string[];
+
+    constructor() {
+        this.hello = ['h', 'e', 'l', 'l', 'o'];
+    }
+
+    doSomething(text: string) {
+        this.hello.push(text);
+    }
+}
---
+++ MinimalApp/Frontend/ngApp/Modules/mainModule.ts
@@ -0,0 +1,13 @@
+﻿// Определение основного angular модуля
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MainComponent } from '../Components/helloWorld/helloWorld';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [MainComponent],
+    bootstrap: [MainComponent]
+})
+export class MainModule { }
